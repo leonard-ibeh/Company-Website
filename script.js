c18279edb72ab08780c21019843a19edd860e312
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-25 20:54:09 +0100

    Added Scroll Button Function

--- script.js
+++ script.js
@@ -31,3 +31,23 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function () {
+  const s1coords = section1.getBoundingClientRect();
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
