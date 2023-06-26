da7816ea393faa2b01ce03838574a8b0b0dd7f6c
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-26 22:52:31 +0100

    Added Sticky Scroll On Navigation Bar

--- script.js
+++ script.js
@@ -126,6 +126,15 @@ const handleHover = function (e) {
 //   handleHover(e, 1);
 // });
 
-// === Passing "argument" into hander====
+// === Passing "argument" into handler ====
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+// =======  Sticky Navigation bar
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
