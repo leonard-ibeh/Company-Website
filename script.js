2748e6cb1a1939d812678b51cdc0b6825994ed79
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-26 23:42:59 +0100

    Enhanced Stick Scroll For Navigation Bar

--- script.js
+++ script.js
@@ -132,9 +132,28 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 
 // =======  Sticky Navigation bar
 
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+//  <======================Sticky navigation Bar  ======================> :
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
